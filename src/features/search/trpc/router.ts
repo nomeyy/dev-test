adcca180ad556f799658456217cfd722a48ba8eb
subodh <subodhdevpatel@gmail.com>
2025-08-06 13:44:04 +0530

    feat: add SSE service with connection manager and event dispatching

--- src/features/search/trpc/router.ts
+++ src/features/search/trpc/router.ts
@@ -1,9 +1,13 @@
 import { createTRPCRouter, publicProcedure } from "@/lib/trpc";
 import { searchUsersHandler } from "./handlers/searchUsers";
-import { SearchUsersInputSchema } from "../types";
+import { SearchUsersInputSchema, brodcastEventInputSchema } from "../types";
+import { brodcastEventHandler } from "./handlers/broadCastEvent";
 
 export const searchRouter = createTRPCRouter({
   users: publicProcedure
     .input(SearchUsersInputSchema)
     .query(searchUsersHandler),
+  broadCast: publicProcedure
+    .input(brodcastEventInputSchema)
+    .mutation(brodcastEventHandler),
 });
